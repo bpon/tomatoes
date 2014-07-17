57c84fbbaad3bfc563829e17e4c4f80b50dbaed7
Bryan Pon <bpon@yahoo-inc.com>
2014-07-16 21:55:02 -0700

    Initial Commit

---
+++ tomatoes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tomatoes
+//
+//  Created by Bryan Pon on 7/16/14.
+//  Copyright (c) 2014 Jab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
